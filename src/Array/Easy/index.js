e8d1aa2c2f477cabed595ba7521668b8c1846377
injamamulhak0804 <injamamulhak080404@gmail.com>
2024-12-02 12:24:11 +0530

    feat:Array_Easy

--- src/Array/Easy/index.js
+++ src/Array/Easy/index.js
@@ -37,3 +37,50 @@
 //     return true
 // }
 // console.log(ifSorted([1,2,2]));
+
+// function LRotateByOne(arr){
+//     let temp = arr[0]
+//     for(let i = 1; i < arr.length; i++){
+//         arr[i - 1] = arr[i]
+//     }
+//     arr[arr.length - 1] = temp
+//     return arr
+// }
+// console.log(LRotateByOne([1,2,3,4,5]))
+
+
+// function LRotateByD(arr, k){
+//     let temp = []
+//     let n = arr.length
+//     k = k%n 
+//     console.log(k);
+    
+//     for(let i = 0; i < k; i++){
+//         temp.push(arr[i])
+//     }
+//     for(let i = k; i < n; i++){
+//         arr[i - k] = arr[i]
+//     }
+//     for(let i = n - k; i < n; i++){
+//         arr[i] = temp[i - (n-k)]
+//     }
+//     console.log(arr);
+    
+// }
+// LRotateByD([1,2,3,4,5],4)
+
+function RRotateByD(nums, k){
+    let n = nums.length    
+    k = k%n
+    let temp = nums.slice(n - k);
+    
+    for (let i = n - 1; i >= k; i--) {
+        nums[i] = nums[i - k];
+    }
+    for(let i = 0 ; i < k; i++){
+        nums[i] =  temp[i]
+    }
+    console.log(nums);
+    
+}
+RRotateByD([1,2,3,4,5,6,7], 3)
